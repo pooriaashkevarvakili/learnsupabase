b84b5ced9814b334caec8ae42c8d04b41099c4b3
pooriavakili09 <pooriavakili09@gmail.com>
2023-01-25 15:46:54 +0330

    delete login
    delete login

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Create from "./components/Create";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import Update from "./components/Update";
-import supabase from "./config/supabaseClient";
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Auth supabaseClient={supabase} appearance={ThemeSupa}>
 
-        <Navbar />
-        <Routes>
-          <Route path="/login" element={<Login />} />
+      <Navbar />
+      <Routes>
 
 
-          <Route path="/" element={<Home />} />
-          <Route path='/create' element={<Create />} />
-          <Route path='/:id' element={<Update />} />
-        </Routes>
-      </Auth>
+
+        <Route path="/" element={<Home />} />
+        <Route path='/create' element={<Create />} />
+        <Route path='/:id' element={<Update />} />
+      </Routes>
+
     </BrowserRouter>
   );
 }
